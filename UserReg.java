6ad6ddbeb65879b6c508ccecd569feb49e368e59
Tejeswar0475 <tejeswar096@gmail.com>
2021-01-01 14:15:15 +0530

    Added code for the firstname

--- UserReg.java
+++ UserReg.java
@@ -8,11 +8,12 @@ public class UserReg {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration");
-		
+
 		System.out.println("Enter First Name:");
+
 		Scanner scanner=new Scanner(System.in);
 		String userInput=scanner.next();
-		
+
 		Pattern firstNameRegExp=Pattern.compile("(^[A-Z]{1})[a-z]{2,}");
 		Matcher matchResult=firstNameRegExp.matcher(userInput);
 		boolean firstNameResult=matchResult.matches();
@@ -24,6 +25,25 @@ public class UserReg {
 		{
 			System.out.println(firstNameResult);
 		}
+
+		System.out.println("Enter Last Name:");
+
+		Scanner scanner1=new Scanner(System.in);
+		String userInputLastName=scanner1.next();
+
+		Pattern lastNameRegExp=Pattern.compile("([A-Z]{1}[a-z]{2,}\s{0,}[A-Z]{1}[a-z]{2,})");
+		Matcher matchResultLastName=lastNameRegExp.matcher(userInputLastName);
+		boolean lastNameResult=matchResultLastName.matches();
+		if(lastNameResult == true)
+		{
+			System.out.println(lastNameResult);
+		}
+		else
+		{
+			System.out.println(lastNameResult);
+		}
+
 	}
 
 }
+
